7e44d95fe474d8a0a29b89e5bb7f43023a039b81
yasiru95 <yrandika0103@gmail.com>
2026-06-10 13:38:40 +0100

    change domain name

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://132.145.72.192/api',
+    baseURL: 'https://kidsstoryflix.co.uk/api',
     headers: {
         'Content-Type': 'application/json'
     }
